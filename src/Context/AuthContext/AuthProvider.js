0e59e2cd2b45061e0faa598e9ed89ada6366041b
wpmdalamin <mdalamin7g@gmail.com>
2022-10-26 20:17:11 +0600

    header coponents added

--- src/Context/AuthContext/AuthProvider.js
+++ src/Context/AuthContext/AuthProvider.js
@@ -10,30 +10,36 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const GoogleAuth = Provider => {
+        setLoading(true);
         return signInWithPopup(auth, Provider);
     }
     const emailAndPasswordAuth = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword( auth, email, password);
     }
     const singIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
     const logOur = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
     useEffect( () => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser)=> {
             setUser(currentUser);
+            setLoading(false)
         })
         return () => {
             unsubscribe();
         }
     }, [])
 
-    const Authinfo = {user , GoogleAuth, emailAndPasswordAuth, logOur, singIn}
+    const Authinfo = {loading, user, GoogleAuth, emailAndPasswordAuth, logOur, singIn}
 
     return (
         <div>
